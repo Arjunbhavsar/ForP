59b61b75f790f6c44dda580aecb83bdab17ab0db
arjunbhavsar <bhavsararjun@gmail.com>
2026-07-05 14:52:56 -0700

    Fix background music asset pathing for subfolder

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import MemoryLane from './MemoryLane';
 import Gallery from './Gallery';
@@ -9,6 +10,18 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 <React.StrictMode> 
+  <HashRouter> 
+    <Routes> 
+      <Route path="/" element={<App />} /> 
+      <Route path="/memory-lane" element={<MemoryLane />} /> 
+      <Route path="/gallery" element={<Gallery />} /> 
+      <Route path="/date-game" element={<DateGame />} /> 
+    </Routes> 
+  </HashRouter> 
+</React.StrictMode>
+);
+
+{/* <React.StrictMode> 
   <BrowserRouter basename="/ForP"> 
     <Routes> 
       <Route path="/" element={<App />} /> 
@@ -17,6 +30,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Route path="/date-game" element={<DateGame />} /> 
     </Routes> 
   </BrowserRouter> 
-</React.StrictMode>
-
-);
+</React.StrictMode> */}
